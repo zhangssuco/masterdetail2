a91780beaaab573b779c937fa51c2de04b10c2ae
zhangs <zhangsgm@gmail.com>
2019-11-03 21:31:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/masterdetail2/addItem.java
@@ -0,0 +1,17 @@
+package com.example.masterdetail2;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class addItem extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.arecord);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/masterdetail2/dummy/sunycampuses.java
@@ -0,0 +1,90 @@
+package com.example.masterdetail2.dummy;
+
+import com.example.masterdetail2.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class sunycampuses {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static final List<campus> ITEMS = new ArrayList<campus>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static final Map<String, campus> ITEM_MAP = new HashMap<String, campus>();
+
+    private static final int COUNT =25;
+
+    static {
+
+        // Add some sample items.
+
+        addItem(new campus("SUCO", "SUNY Oneonta", "A liberal art college", R.drawable.suco));
+
+        addItem(new campus("Albany", "SUNY Albany", "A research center", R.drawable.albany));
+
+        addItem(new campus("Binghamton", "SUNY Binghamton", "A research", R.drawable.suco));
+
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createDummyItem(i));
+        }
+
+
+
+
+    }
+
+    private static void addItem(campus item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static campus createDummyItem(int position) {
+        return new campus(String.valueOf(position), "Item " + position, makeDetails(position), R.drawable.suco);
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class campus {
+        public final String id;
+        public final String content;
+        public final String details;
+        public final int logoid;
+
+        public campus(String id, String content, String details, int logoid) {
+            this.id = id;
+            this.content = content;
+            this.details = details;
+            this.logoid=logoid;
+
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+
+    }
+}
